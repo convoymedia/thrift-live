977a7ce48bb2ac7926f189a75a38f0234cee2c1c
convoymedia-paul <paul@convoymedia.com>
2020-05-04 15:14:13 +0100

    remove not needed

--- content_blocks/image_left_text_right.php
+++ content_blocks/image_left_text_right.php
@@ -45,30 +45,6 @@
         <h2><?php the_sub_field("title"); ?></h2>
         <?php the_sub_field("text"); ?>
         <?php 
-                if (get_sub_field("include_button")) { ?>
-                    
-                    <?php 
-                        if (get_sub_field("button_gradient_or_colour") !== "gradient") {
-                            ?>
-                            <a href="<?php the_sub_field("button_link"); ?>">
-                                <button style="background-color:<?php the_sub_field("button_colour"); ?>;color:<?php the_sub_field("button_text_colour"); ?>">
-                                    <?php the_sub_field("button_title"); ?>
-                                </button>
-                            </a>
-                            <?php
-                        }
-                        else {
-                            ?>
-                            <a href="<?php the_sub_field("button_link"); ?>">
-                                <button style="background: <?php the_sub_field("button_start_colour"); ?>;background: linear-gradient(94deg, <?php the_sub_field("button_start_colour"); ?> 0%, <?php the_sub_field("button_end_colour"); ?> 100%);color:<?php the_sub_field("button_text_colour"); ?>">
-                                    <?php the_sub_field("button_title"); ?>
-                                </button>
-                            </a>
-                            <?php
-                        }
-                    }
-            ?>
-        <?php 
             if (!get_sub_field("add_2nd_colour")) { 
                 if (get_sub_field("include_button")) { ?>
                     <?php if (get_sub_field("button_gradient_or_colour") !== "gradient") { ?>
