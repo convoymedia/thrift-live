870441badff0dedbd6e5506d67f9b0593defe0b6
convoymedia-paul <paul@convoymedia.com>
2020-05-14 15:04:54 +0100

    box

---
+++ content_blocks/text_block_centered.php
@@ -0,0 +1,3 @@
+<div class="text_block_centered" style="backgrond-color:<?php the_sub_field("bg_colour"); ?>">
+    <div class="box" style="background-color:<?php the_sub_field("box_colour"); ?>"><?php the_sub_field("text"); ?></div>
+</div>
