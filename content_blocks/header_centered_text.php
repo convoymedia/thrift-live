fc16515636db43c303fe7e0b522133b74d148e85
convoymedia-paul <paul@convoymedia.com>
2020-05-14 14:27:00 +0100

    the to get

--- content_blocks/header_centered_text.php
+++ content_blocks/header_centered_text.php
@@ -1,3 +1,3 @@
 <div class="header-centered-text" style="background-image:url(<?php the_sub_field("background_image"); ?>)">
-    <?php the_sub_field("text"); ?>
+    <?php echo get_sub_field("text"); ?>
 </div>
