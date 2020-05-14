f9427857d4f7a00b389fc373409cfe4a2896d800
convoymedia-paul <paul@convoymedia.com>
2020-05-14 14:32:53 +0100

    text center

--- content_blocks/header_centered_text.php
+++ content_blocks/header_centered_text.php
@@ -1,3 +1,3 @@
 <div class="header-centered-text" style="background-image:url(<?php the_sub_field("background_image"); ?>)">
-    <?php echo str_replace(array("<p>", "</p>"), "", get_sub_field("text")); ?>
+    <?php $text = get_sub_field("text"); ?>
 </div>
